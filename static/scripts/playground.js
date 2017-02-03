0c5bb29b21043aac6bc1c65fcb7c504cd4412314
Dmitry Veselov <d.a.veselov@yandex.ru>
2017-02-03 21:53:34 +0300

    Brand new website version

--- static/scripts/playground.js
+++ static/scripts/playground.js
@@ -11,49 +11,63 @@
         });
     };
 
-    var NatashaEndpoint = 'https://natasha-playground.herokuapp.com/api/extract';
-    var GrammarsClasses = {
-        Person: 'person',
-        Organisation: 'org',
-        Geo: 'geo',
-        Date: 'date',
-        Money: 'money',
-        Event: 'event',
-        Brand: 'brand',
+    var ExtractEndpoint = 'https://natasha-playground.herokuapp.com/api/extract',
+        VersionEndpoint = 'https://natasha-playground.herokuapp.com/api/version',
+        StartAnalysisButton = document.getElementById('start-analysis');
+
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (request.readyState == 4 && request.status == 200) {
+            var element = document.getElementById('version');
+                versions = JSON.parse(request.responseText);
+            element.innerHTML = versions.yargy + ' / ' + versions.natasha;
+        };
     };
-    var form = document.forms[0].addEventListener('submit', function(e) {
+    request.open('GET', VersionEndpoint, true);
+    request.send();
+
+    StartAnalysisButton.addEventListener('click', function(e) {
         e.preventDefault();
         var request = new XMLHttpRequest(),
-            text = this.text.value.escape(),
-            button = this.start;
-        button.className = button.className.replace('pure-button-primary', 'pure-button-disabled');
+            container = document.getElementById('display')
+            text = container.textContent.replace(/\n/g, ' '),
+            button = this;
+        button.className = button.className.replace('button-default', 'button-disabled');
         request.onreadystatechange = function() {
             if (request.readyState == 4 && request.status == 200) {
                 var matches = JSON.parse(request.responseText),
-                    originals = [];
-                matches = matches.sort(function(a, b) {
-                    return b.tokens.length - a.tokens.length;
-                });
+                    replaced = [],
+                    ents = [];
+
+                console.log(matches);
+
                 for (var i = matches.length - 1; i >= 0; i--) {
                     var grammar = matches[i].grammar,
                         tokens = matches[i].tokens;
                     var position = [tokens[0].position[0], tokens[tokens.length - 1].position[1]];
                     var original = text.substring(position[0], position[1]);
-                    originals.push([grammar, original, position]);
+                    ents.push({
+                        type: grammar,
+                        start: position[0],
+                        end: position[1],
+                        text: original,
+                    });
+                }
+
+                for (var i = ents.length - 1; i >= 0; i--) {
+                    if (replaced.indexOf(ents[i].text) === -1) {
+                        replaced.push(ents[i].text);
+                        console.log(ents[i].type);
+                        text = text.split(ents[i].text).join('<mark data-entity="' + ents[i].type.toLowerCase() + '">' + ents[i].text + '</mark>');
+                    };
                 };
-                for (var i = originals.length - 1; i >= 0; i--) {
-                    var grammar = originals[i][0],
-                        original = originals[i][1],
-                        position = originals[i][2];
-                    text = text.split(original).join('<span class="' + GrammarsClasses[grammar] + '">' + original + '</span>');
-                };
-                text = text.split('\n').join('<br>');
-                document.getElementById("result").innerHTML = text;
-                button.className = button.className.replace('pure-button-disabled', 'pure-button-primary');
+
+                container.innerHTML = text;
+                button.className = button.className.replace('button-disabled', 'button-default');
             };
         };
-        request.open("POST", NatashaEndpoint, true);
-        request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        request.send("text=" + text);
+        request.open('POST', ExtractEndpoint, true);
+        request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        request.send('text=' + encodeURIComponent(text));
     });
 })();
